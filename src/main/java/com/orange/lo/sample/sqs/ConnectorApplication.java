7ed68091547f6b95e8b0201554e534cacee4ac66
grzegorzswistak <grzegorz.swistak2@gmail.com>
2023-01-11 10:48:17 +0100

    Revert "Edit v1 (#8)"
    This reverts commit 8a4c1900b0cfb0e22eae15d18b415d910f9dcb71.

--- src/main/java/com/orange/lo/sample/sqs/ConnectorApplication.java
+++ src/main/java/com/orange/lo/sample/sqs/ConnectorApplication.java
@@ -7,29 +7,13 @@
 
 package com.orange.lo.sample.sqs;
 
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ConnectorApplication {
 
-    private static ConfigurableApplicationContext context;
-    
     public static void main(String[] args) {
-        context = SpringApplication.run(ConnectorApplication.class, args);
-    }
-    
-    public static void restart() {
-        ApplicationArguments args = context.getBean(ApplicationArguments.class);
-
-        Thread thread = new Thread(() -> {
-            context.close();
-            context = SpringApplication.run(ConnectorApplication.class, args.getSourceArgs());
-        });
-
-        thread.setDaemon(false);
-        thread.start();
+        SpringApplication.run(ConnectorApplication.class, args);
     }
 }
