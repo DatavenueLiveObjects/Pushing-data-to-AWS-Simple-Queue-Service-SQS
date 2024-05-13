1b465aaa25addf81a5d889b56e73fb8bdb773010
Szymon Lisiecki <szymon.lisiecki@likims.com>
2024-05-13 06:39:36 +0200

    add cloudwatch metrics - fix tests

--- src/main/java/com/orange/lo/sample/sqs/ConnectorApplication.java
+++ src/main/java/com/orange/lo/sample/sqs/ConnectorApplication.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConnectorApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConnectorApplication.class, args);
     }
